eb732b1879efa9f48892abbb3e02d1aa0283e464
Carlos <chenrique452@gmail.com>
2022-03-31 09:05:32 -0300

    JSON fixed

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 
 
 const backgroundImagesLocation = [
-    '../images/icon-work.svg',
-    '../images/icon-play.svg',
-    '../images/icon-study.svg',
-    '../images/icon-exercise.svg',
-    '../images/icon-social.svg',
-    '../images/icon-self-care.svg'
+    './images/icon-work.svg',
+    './images/icon-play.svg',
+    './images/icon-study.svg',
+    './images/icon-exercise.svg',
+    './images/icon-social.svg',
+    './images/icon-self-care.svg'
 ]
 
 
